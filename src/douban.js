b2af3b27e81b2a709558788b29269446b7b7dafa
4wangyu <4wangyu@gmail.com>
2021-05-23 16:40:07 +0800

    add imdb link

--- src/douban.js
+++ src/douban.js
@@ -1,11 +1,41 @@
-const movieId = window.location.pathname.split('/')[2];
+function addPlayButton() {
+  const movieId = window.location.pathname.split('/')[2];
 
-const douban = document.createElement('a');
-douban.innerHTML = '&#9658;';
-douban.setAttribute('style', 'margin-left: 10px;');
-douban.href = 'https://xinghe.tv/play/' + movieId;
-douban.target = '_blank';
-douban.rel = 'noreferrer noopener';
+  const douban = document.createElement('a');
+  douban.innerHTML = '&#9658;';
+  douban.setAttribute('style', 'margin-left: 10px;');
+  douban.href = 'https://xinghe.tv/play/' + movieId;
+  douban.target = '_blank';
+  douban.rel = 'noreferrer noopener';
 
-const year = document.querySelector('#content .year');
-year.parentNode.insertBefore(douban, year.nextSibling);
+  const year = document.querySelector('#content .year');
+  year.parentNode.insertBefore(douban, year.nextSibling);
+}
+
+function addImdbLink() {
+  const xpath = "//span[text()='IMDb:']";
+  const matchingElement = document.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+  if (matchingElement) {
+    const imdbId = matchingElement.nextSibling.textContent.trim();
+    matchingElement.nextSibling.remove();
+    const imdbLink = document.createElement('a');
+    imdbLink.innerText = imdbId;
+    imdbLink.setAttribute('style', 'margin-left: 4px;');
+    imdbLink.href = 'https://www.imdb.com/title/' + imdbId;
+    imdbLink.target = '_blank';
+    imdbLink.rel = 'noreferrer noopener';
+    matchingElement.parentNode.insertBefore(
+      imdbLink,
+      matchingElement.nextSibling
+    );
+  }
+}
+
+addPlayButton();
+addImdbLink();
